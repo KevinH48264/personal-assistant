fee20f96b869b921bbf0d36bb401ab1e8561fce4
KevinH48264 <1kevin.huang@gmail.com>
2020-01-13 16:18:39 -0500

    Updates

---
+++ precache-manifest.82d311d04649b5549d8afec3dd6fc938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1b6d5c1fb8a90a5fabe3605134dff5",
+    "url": "/personal-assistant/index.html"
+  },
+  {
+    "revision": "93199f82deedf51f7903",
+    "url": "/personal-assistant/static/css/main.c869ab27.chunk.css"
+  },
+  {
+    "revision": "e648798d19dec0a52dfd",
+    "url": "/personal-assistant/static/js/2.aaa4e16f.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/personal-assistant/static/js/2.aaa4e16f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93199f82deedf51f7903",
+    "url": "/personal-assistant/static/js/main.6c4ca8eb.chunk.js"
+  },
+  {
+    "revision": "6af68dc79e5ea81f8987",
+    "url": "/personal-assistant/static/js/runtime-main.9f1a1dcf.js"
+  }
+]);
